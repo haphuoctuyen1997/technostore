ecefb9afb10977bf720e41ed110e0c85f19bfdc4
haphuoctuyen1997 <haphuoctuyen1997@gmail.com>
2018-08-29 21:07:18 +0700

    create_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,17 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
   protect_from_forgery with: :exception
+
   before_action :set_locale
+
+  rescue_from ActiveRecord::RecordNotFound, NoMethodError, with: :not_found?
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def not_found?
+    render file: "#{Rails.root}/public/404.html", status: 403, layout: false
   end
 
   private
