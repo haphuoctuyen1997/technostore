5b294b6d9df66c151c11deac54a7a0f07c1161c0
haphuoctuyen1997 <haphuoctuyen1997@gmail.com>
2018-09-10 13:09:27 +0700

    history_order_hand

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  before_action :category_all, only: :new
+
   def new; end
 
   def create
--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -2,4 +2,8 @@ module OrdersHelper
   def day_format
     Date.today.strftime(" %d-%m-%Y ")
   end
+
+  def created_at_format date
+    date.strftime("%I:%M %p %d-%m-%Y")
+  end
 end
