ea4d7ffe99e9bdc46b0e073fe73abb7e174a13da
haphuoctuyen1997 <haphuoctuyen1997@gmail.com>
2018-09-10 09:11:14 +0700

    3.2menu category

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
+  before_action :category_all, only: :home
+
   def home
     @top_order_products = Product.top_order
     @products = Product.includes(:category).paginate page: params[:page],
